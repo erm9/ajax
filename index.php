84003b13a26aeac811707a6a9bb12a3f6d05a5e3
Bill Newman <william.newman@seattlecolleges.edu>
2022-02-10 19:28:46 +0000

    Shows and hides elements

--- index.php
+++ index.php
@@ -44,6 +44,15 @@
 <script>
     $("document").ready(function(){
         
+        //hide likes and eats
+        $('#pet_likes').hide();
+        $('#pet_eats').hide();
+
+        $('#pet_feels').click(function(e){
+          $('#pet_likes').slideDown(200);
+
+        });
+
         
         $('#myForm').submit(function(e){
             e.preventDefault();//no need to submit as you'll be doing AJAX on this page
